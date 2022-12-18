b0d499024be2d81faa9af31c8a78fcf42fc2310a
Estevão Henrique <113246534+Estevao15@users.noreply.github.com>
2022-12-18 00:15:41 -0300

    Add files via upload

---
+++ assets/js/conta bancaria.js
@@ -0,0 +1,9 @@
+let saldo = prompt("digite seu saldo")
+let produto = prompt("Digite o valor do produto")
+
+if(saldo>produto) {
+    alert('Saldo suficiente!')
+}
+else if (saldo<produto) {
+    alert('saldo insuficiente!')
+}
